2b1342ea6a9c6524f7753c0b5c260a15891be553
chalacol <ese.chala.amenu@gmail.com>
2021-08-26 01:49:26 -0700

    proper grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-} 
+}
