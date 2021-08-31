7725dacfe3b412a697097d51483ccc5701c47ed9
chalacol <ese.chala.amenu@gmail.com>
2021-08-31 07:14:48 -0700

    help please

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char holberton[8] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	char holberton[10] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 	unsigned int i;
 
 	for (i = 0; i < sizeof(holberton); i++)
