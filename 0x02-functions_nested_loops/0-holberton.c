ac283875a9c199219b06308eb7cb8fff0cd9eacd
chalacol <ese.chala.amenu@gmail.com>
2021-08-31 06:53:46 -0700

    adding _putchar and main.h

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,23 +1,22 @@
 #include "main.h"
+#include "holberton.h"
 
 /**
- * main - main block
- * Description: Get a random number and print the number
- * and if it is positive, negative, or zero
- * Return: 0
+ * main - Entry point, prints ""Holberton" to the standard output
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n < 0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n);
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar(10);
 	return (0);
 }
