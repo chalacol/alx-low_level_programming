c35061ae882e02c8fd75d57ac051dc67e78fae2e
chalacol <ese.chala.amenu@gmail.com>
2021-08-31 04:45:45 -0700

    rite a program that prints Holberton, followed by a new line.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
+
 /**
- * main - Entry block
- * Description: prints holberton on new line
- * Return: 0
+ * main - Entry point
+ * Return:0
  */
+
 int main(void)
 {
-	char c[10] = "Holberton";
-
-	int i = 0;
-
-	while (i < 9)
-	{
-		_putchar(c[i]);
-		i++;
-	}
-	_putchar('\n');
-	return (0);
+char c[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
