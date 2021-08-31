1a758865d44c7b38ea8508cbe70eda86c29f121a
chalacol <ese.chala.amenu@gmail.com>
2021-08-31 07:00:26 -0700

    _putchar and main.h

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * main - Entry point, prints ""Holberton" to the standard output
+ * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar(10);
+	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	unsigned int c;
+
+	for (c = 0; c < sizeof(holberton); c++)
+	{
+		_putchar(holberton[c]);
+	}
+	_putchar('\n');
 	return (0);
 }
